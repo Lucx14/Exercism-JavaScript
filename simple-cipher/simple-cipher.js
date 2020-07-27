42d8f9c35b9deec0e1098d383776ee09957824e5
Lucien Najev <luciennajev@hotmail.com>
2020-07-27 15:48:15 +0100

    new ex

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,35 +1,31 @@
-const generateRandomKey = (length) => {
-  let result = '';
-  const characters = 'abcdefghijklmnopqrstuvwxyz';
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
-
-const validateKey = (key) => {
-  const regex = /^[a-z]+$/;
-  if (!regex.test(key)) {
-    throw new Error('Bad key');
-  }
-};
-
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
 export class Cipher {
-  constructor(key = generateRandomKey(100)) {
-    validateKey(key);
+  constructor(key = Cipher.generateRandomKey(100)) {
+    Cipher.validateKey(key);
     this.key = key;
+  }
+
+  static get ALPHABET() { return 'abcdefghijklmnopqrstuvwxyz'; }
+
+  static validateKey(key) {
+    if (!/^[a-z]+$/.test(key)) throw new Error('Bad key');
+  }
+
+  static generateRandomKey(length) {
+    let result = '';
+    for (let i = 0; i < length; i += 1) {
+      result += Cipher.ALPHABET.charAt(Math.floor(Math.random() * this.ALPHABET.length));
+    }
+    return result;
   }
 
   encode(plaintext) {
     const letters = plaintext.split('');
     const result = [];
 
-    for (let i = 0; i < letters.length; i += 1) {
-      const letterIndex = alphabet.indexOf(letters[i]);
-      const diff = alphabet.indexOf(this.key[i % this.key.length]);
-      result.push(alphabet[(letterIndex + diff) % 26]);
+    for (let index = 0; index < letters.length; index += 1) {
+      const letterIndex = Cipher.ALPHABET.indexOf(letters[index]);
+      const diff = Cipher.ALPHABET.indexOf(this.key[index % this.key.length]);
+      result.push(Cipher.ALPHABET[(letterIndex + diff) % 26]);
     }
 
     return result.join('');
@@ -39,11 +35,11 @@ export class Cipher {
     const letters = ciphertext.split('');
     const result = [];
 
-    for (let i = 0; i < letters.length; i += 1) {
-      const letterIndex = alphabet.indexOf(letters[i]);
-      const diff = alphabet.indexOf(this.key[i]);
+    for (let index = 0; index < letters.length; index += 1) {
+      const letterIndex = Cipher.ALPHABET.indexOf(letters[index]);
+      const diff = Cipher.ALPHABET.indexOf(this.key[index]);
       const newIndex = letterIndex - diff < 0 ? (letterIndex - diff) + 26 : letterIndex - diff;
-      result.push(alphabet[newIndex]);
+      result.push(Cipher.ALPHABET[newIndex]);
     }
 
     return result.join('');
