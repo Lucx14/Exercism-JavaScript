4fe83de3bd5f38eb2e15a0398e93edaf88603f45
Lucien Najev <luciennajev@hotmail.com>
2020-07-29 15:59:28 +0100

    new ex

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,4 +1,6 @@
 const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const alphabetIndex = letter => ALPHABET.indexOf(letter);
+const newIndex = (oldIndex, shift) => ((oldIndex + shift) + ALPHABET.length) % ALPHABET.length;
 
 export class Cipher {
   constructor(key = Cipher.generateRandomKey(100)) {
@@ -20,26 +22,20 @@ export class Cipher {
   }
 
   encode(plaintext) {
-    return this.convert(plaintext, 'encode');
+    return this.convert(plaintext, 1);
   }
 
   decode(ciphertext) {
-    return this.convert(ciphertext, 'decode');
+    return this.convert(ciphertext, -1);
   }
 
   convert(text, direction) {
-    const letters = text.split('');
-    const result = [];
+    return text.split('')
+      .map((_, i) => ALPHABET[newIndex(alphabetIndex(text[i]), (this.shift(i, direction)))])
+      .join('');
+  }
 
-    for (let index = 0; index < letters.length; index += 1) {
-      const letterIndex = ALPHABET.indexOf(letters[index]);
-      const diff = ALPHABET.indexOf(this.key[index % this.key.length]);
-      const newIndex = direction === 'encode'
-        ? (letterIndex + diff) % ALPHABET.length
-        : (((letterIndex - diff) + ALPHABET.length) % ALPHABET.length);
-      result.push(ALPHABET[newIndex]);
-    }
-
-    return result.join('');
+  shift(index, direction) {
+    return ALPHABET.indexOf(this.key[index % this.key.length]) * direction;
   }
 }
