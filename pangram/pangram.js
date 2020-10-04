27db881fafe876935dac3e91141e002276542350
Lucien Najev <luciennajev@hotmail.com>
2020-10-04 12:25:30 +0100

    refactor ex

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,7 +1,10 @@
 const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 
-export const isPangram = (text) => {
-  const chars = text.toLowerCase().split('').sort();
-  const lettersUsed = [...new Set(chars)].filter(char => char.match(/[a-z]/)).join('');
-  return lettersUsed === ALPHABET;
-};
+export const isPangram = (text) => [
+    ...new Set(
+      text
+        .toLowerCase()
+        .split('')
+        .filter(char => char.match(/[a-z]/))
+    )
+  ].length === ALPHABET.length;
