c9c0ddeacaa77e3b7526021d0f2e4237c931086b
Lucien Najev <luciennajev@hotmail.com>
2021-02-01 16:49:46 +0000

    new ex

---
+++ bank-account/bank-account.js
@@ -0,0 +1,63 @@
+'use strict';
+
+export class BankAccount {
+  constructor() {
+    this.accountOpen = false;
+  }
+
+  open() {
+    if (this.accountOpen) {
+      throw new ValueError("Account already open");
+    } else {
+      this.accountOpen = true;
+      this.accountBalance = 0;
+    }
+  }
+
+  close() {
+    if (this.accountOpen) {
+      this.accountOpen = false;
+      this.accountBalance = null;
+    } else {
+      throw new ValueError("Account is closed");
+    }
+  }
+
+  deposit(amount) {
+    if (this.accountOpen) {
+      if (amount >= 0) {
+        this.accountBalance += amount;
+      } else {
+        throw new ValueError("Cannot deposit a negative amount");
+      }
+    } else {
+      throw new ValueError("Account is not open");
+    }
+  }
+
+  withdraw(amount) {
+    if (this.accountOpen) {
+      if (amount < 0 || amount > this.balance) {
+        throw new ValueError("Invalid amount");
+      } else {
+        this.accountBalance -= amount;
+      }
+    } else {
+      throw new ValueError("Account is not open");
+    }
+  }
+
+  get balance() {
+    if (this.accountOpen) {
+      return this.accountBalance;
+    } else {
+      throw new ValueError("Account is not open");
+    }
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super('Bank account error');
+  }
+}
---
+++ matrix/matrix.js
@@ -0,0 +1,13 @@
+export class Matrix {
+  constructor(str) {
+    this.grid = str.split('\n').map(x => x.split(' ').map(y => parseInt(y)));
+  }
+
+  get rows() {
+    return this.grid;
+  }
+
+  get columns() {
+    return this.grid[0].map((_, i) => this.grid.map(row => row[i]));
+  }
+}
