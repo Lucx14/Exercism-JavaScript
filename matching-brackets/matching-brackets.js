c79748f4541e922cbfb5d9e1cbd78f9df2d2c698
Lucien Najev <luciennajev@hotmail.com>
2021-02-03 23:33:27 +0000

    new ex

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -1,10 +1,27 @@
 'use strict';
 
-export const isPaired = (str) => {
-  console.log(str.replace(/^\\(/g, ''));
-  console.log([...str]);
-  return true;
-  // throw new Error('Remove this statement and implement this function');
+const BRACKETS = {
+  '(': ')',
+  '{': '}',
+  '[': ']'
 };
 
-// []  {}  ()
+const openingBracket = char => Object.keys(BRACKETS).includes(char);
+const closingBracket = char => Object.values(BRACKETS).includes(char);
+
+export const isPaired = (str) => {
+  const brackets = [...str].filter(x => [...'({[]})'].includes(x));
+  const stack = [];
+
+  for (const br of brackets) {
+    if (openingBracket(br)) stack.push(br);
+    if (closingBracket(br)) {
+      const lastEl = stack.pop();
+      if (!lastEl || BRACKETS[lastEl] !== br) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; 
+};
