5dfc3407b8a532187c186c62cef936da04fdae77
Lucien Najev <luciennajev@hotmail.com>
2021-02-03 19:41:51 +0000

    new ex

---
+++ matching-brackets/matching-brackets.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export const isPaired = (str) => {
+  console.log(str.replace(/^\\(/g, ''));
+  console.log([...str]);
+  return true;
+  // throw new Error('Remove this statement and implement this function');
+};
+
+// []  {}  ()
