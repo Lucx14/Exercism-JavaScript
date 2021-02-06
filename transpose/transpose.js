be35f83fb59a5f7002c643aa809397bbf3a20ebb
Lucien Najev <luciennajev@hotmail.com>
2021-02-06 22:29:51 +0000

    New exercise

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -1,8 +1,27 @@
-//
-// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+"use strict";
 
-export const transpose = () => {
-  throw new Error('Remove this statement and implement this function');
+export const transpose = (arr) => {
+  if (arr.length === 0) {
+    return [];
+  }
+  // throw new Error("Remove this statement and implement this function");
+  const result = [];
+
+  const x = arr.map((x) => [...x]);
+  const longestIndex = Math.max(...x.map((x) => x.length));
+  for (let xx = 0; xx < longestIndex; xx++) {
+    result.push(x.map((ss) => ss[xx]));
+  }
+  const qqq = result.map((aa) => {
+    return aa.map((xxx) => {
+      if (xxx === undefined) {
+        return " ";
+      } else {
+        return xxx;
+      }
+    });
+  });
+  const hhhh = qqq.map((x) => x.join(""));
+  hhhh[hhhh.length - 1] = hhhh[hhhh.length - 1].trimEnd();
+  return hhhh;
 };
