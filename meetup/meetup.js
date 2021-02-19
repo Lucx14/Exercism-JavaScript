464a1f970d83ce1af5ca0425e9ebac1eb5b47fe4
Lucien Najev <luciennajev@hotmail.com>
2021-02-19 16:25:41 +0000

    New ex

--- meetup/meetup.js
+++ meetup/meetup.js
@@ -1,8 +1,34 @@
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+"use strict";
 
-export const meetup = () => {
-  throw new Error('Remove this statement and implement this function');
+export const meetup = (year, month, descriptor, day) => {
+  const teen = (num) => num > 12 && num < 20;
+  const DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const DATE_MAP = {
+    first: 0,
+    second: 1,
+    third: 2,
+    fourth: 3,
+  };
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const dates = [];
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    const date = new Date(year, month - 1, i);
+    if (DAYS[date.getDay()] === day) dates.push(date.getDate());
+  }
+
+  const position =
+    (descriptor === "teenth" && dates.findIndex((date) => teen(date))) ||
+    (descriptor === "last" && dates.length - 1) ||
+    DATE_MAP[descriptor];
+
+  return new Date(year, month - 1, dates[position]);
 };
